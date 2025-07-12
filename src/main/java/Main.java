be62b75b02270497a9bd429db0b498582a8ce40c
sujeeth-sk <sujeeth2004@gmail.com>
2025-07-12 11:50:23 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,85 @@
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String [] args) throws IOException{
+        System.out.println("Logs from your program will appear here!");
+        int port = 6379;
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    // int port = 8000;
-    // while (true) {
-    try {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept();
-      while (true) {
-        // byte[] buffer = new byte[1024];
-        clientSocket.getInputStream().read(new byte[1024]);
-        // String input = new String(buffer).trim();
-        // OutputStream os = clientSocket.getOutputStream();
-        // os.write
+        Selector selector = Selector.open();
+        
+        ServerSocketChannel serverSocket = ServerSocketChannel.open();
+        serverSocket.bind(new InetSocketAddress(port));
+        serverSocket.configureBlocking(false);
+        serverSocket.register(selector, SelectionKey.OP_ACCEPT);  
+        System.out.println("Stared redis server on port " + port);  
 
-        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-      }
-      // serverSocket.close();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        //event loop
+        while(true){
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            // for (SelectionKey key : selectedKeys) {
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+
+            while(iter.hasNext()){
+                SelectionKey key = iter.next();
+
+                //Accepting new cleint connection
+                if(key.isAcceptable()){
+
+
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel client = server.accept();
+                    if(client != null){
+                        client.configureBlocking(false); //non blocking
+                        client.register(selector, SelectionKey.OP_READ);
+                        System.out.println("New client connected " + client.getRemoteAddress());
+                    }
+                } 
+                
+                //Client already exists
+                else if (key.isReadable()){
+                    SocketChannel client = (SocketChannel) key.channel();
+                    buffer.clear(); //reset buffer for new read
+
+                    int bytesRead = client.read(buffer);
+
+                    if(bytesRead == -1){
+                        System.out.println("Client diconnected " + client.getRemoteAddress()); 
+                        client.close();
+                        continue;
+                    }
+
+                    buffer.flip(); //prepare buffer to read data from it
+
+                    if(buffer.remaining() > 0){
+                        byte[] data =  new byte[buffer.remaining()];
+                        buffer.get(data);
+                        String input = new String(data).trim(); //convert byte to string
+                        System.out.println("Recieved from client " + client.getRemoteAddress() + " " + input);
+                    }    
+
+
+                    //static response RESP protocol
+                    String response = "+GONG\r\n";
+                    ByteBuffer responseBuffer = ByteBuffer.wrap(response.getBytes());
+                    client.write(responseBuffer);
+
+
+                }
+            }
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
-    }
-    // }
+        
 
-  }
+    }
+    
 }
