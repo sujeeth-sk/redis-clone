ee7b1a88a12df31ead022148d296a9dca261605d
sujeeth-sk <sujeeth2004@gmail.com>
2025-07-23 19:52:28 +0530

    rdb read support

--- src/main/java/com/example/redisClone/rdb/RDBconfigHandler.java
+++ src/main/java/com/example/redisClone/rdb/RDBconfigHandler.java
@@ -12,6 +12,7 @@ public class RDBconfigHandler {
     public static HashMap<String, RedisStoreObject> loadRDB(RDBconfig rdbConfig) {
         String directory = rdbConfig.get("directory");
         String dataBaseFileName = rdbConfig.get("dataBaseFileName");
+
         File rdbFile = new File(directory, dataBaseFileName);
         if (directory == null || dataBaseFileName == null) {
             System.out.println("Missing --dir or --dbFilename arguments");
@@ -154,68 +155,4 @@ public class RDBconfigHandler {
         }
         return value;
     }
-        if (directory == null || dataBaseFileName == null) {
-            System.out.println("Missing --dir or --dbFilename arguments");
-            return new HashMap<>();
-        }
-        File rdbFile = new File(directory, dataBaseFileName);
-        
-        if(!rdbFile.exists()){
-            System.out.println("Loading RDB from: " + rdbFile.getAbsolutePath());
-            return new HashMap<>();
-        }
-
-        System.out.println("Loading RDB from: " + rdbFile.getAbsolutePath());
-        HashMap<String, RedisStoreObject> store = new HashMap<>();
-
-        try(FileInputStream fis = new FileInputStream(rdbFile) ; DataInputStream dis = new DataInputStream(fis)){
-            byte [] magic = new byte[5];
-            dis.readFully(magic);
-
-            if(!"REDIS".equals(new String(magic))){
-                throw new IOException("Invalid RDB file: Magic string does not match 'REDIS'");
-            }
-            dis.skipBytes(4);
-
-            //parse opcode and data until EOF
-
-            int opcode;
-            while((opcode = dis.read()) != -1){
-                if(opcode == 0xFF){ //end of file
-                    break;
-                }
-
-                if(opcode == 0xFE){ //database selector
-                    readLength(dis);
-                    continue;
-                }
-
-                if(opcode == 0xFB){ // resizedb
-                    readLength(dis); //hash table size 
-                    readLength(dis); //expore hash table size
-                    continue;
-                }
-
-                if(opcode == 0xFA){ // aux field
-                    readString(dis); //key
-                    readString(dis); //value
-                    continue;    
-                }
-
-                if(opcode == 0){
-                    String key = readString(dis);
-                    String value = readString(dis);
-                    store.put(key, new RedisStoreObject(value));
-                }
-
-            }
-            System.out.println("loaded " + store.size() + " keys form rdb");
-            return store;
-
-        } catch (IOException e) {
-            System.out.println("Error loading RDB file: " + e.getMessage());
-            return null;
-        }
-    }
-
 }
